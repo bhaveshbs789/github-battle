7ec4120f43fa63d3ba5ad871be72f7d1cdb1da8e
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-04-27 18:39:31 +0530

    Added Contructor Function , state added for language and method for udpating lnaguage added

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,20 @@
 var React = require('react');
 
 class Popular extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedLanguage : 'All'
+        };
+    }
 
+    updateLanguage(language){
+        this.setState(function(){
+            return {
+                selectedLanguage: language
+            }
+        });
+    }
     render(){
         var languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
         return (
