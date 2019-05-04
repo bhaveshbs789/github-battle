07ab2af31435231e97b4dd27460ae3ec30e6e2cf
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-05-04 18:14:06 +0530

    Added BrowserRouter , updated styles to App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,26 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var BrowserRouter = require('react-router-dom').BrowserRouter;
+var Home = require('./Home');
+var Battle = require('./Battle');
+// var createBrowserHistory = require('react-router-dom').createBrowserHistory;
 
 class App extends React.Component {
     render(){
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <BrowserRouter>
+                <div className='container'>
+                    <Nav />
+                    <Route exact path='/' component={Home} />
+                    <Route path='/battle' component={Battle} />
+                    <Route path='/popular' component={Popular} />
+                </div>
+            </BrowserRouter>
+            
         )
     }
 }
