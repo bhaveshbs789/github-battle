5b1cb6b0005a5143073ffbfd065693d3c88aebe4
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-05-05 16:37:27 +0530

    Added HandleSubmit method to Battlejs component which updates the state accordingly

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -10,10 +10,28 @@ class Battle extends React.Component {
             playerOneImage: null,
             playerTwoImage: null
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+    // For the handleSubmit method we are passing two parameters
+    // id and username. If id is playerOne then update the playerOne 
+    // name and Image or else otherwise.
+    handleSubmit(id, username){
+        this.setState(function(){
+            var newState = {
+            }
+            newState[id + 'Name'] = username;
+            newState[id + 'Image'] = 'https://github.com/' + uername + '.png?size=200';
+            return newState;
+        })
+    }
+
     render(){
         return (
-            <div>Battle</div>
+            <div>
+                <div className="row">
+                    
+                </div>
+            </div>
         )
     }
 }
