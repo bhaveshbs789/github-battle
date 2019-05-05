9fab4495794f2f6e7b81abfd4f5ac36c8b0a9464
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-05-05 17:18:03 +0530

    Added PLayerInput component with Form element and styling to it

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,56 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // triggered when the input field value changes
+    handleChange(event){
+        var value = event.target.value;
+        this.setState(function(){
+            return {username: value}            
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault(); // prevent default to NOT send it to some server
+        // The below onSubmit is called from the props passed to PlayerInput component
+        // which is called with the props.id and the username from the ste property in 
+        //PlayerInput component.
+        this.props.onSubmit(this.props.id, this.state.username)
+    }
+
+    render(){
+        return (
+            <form className='column' onSubmit={this.handleSubmit}>
+                <label className='header' htmlFor='username'>{this.props.label}</label>
+                <input id='username' type='text' 
+                    placeholder='github username' 
+                    autoComplete='off' 
+                    value={this.state.username}
+                    onChange={this.handleChange}></input>
+                    <button
+                        className='button'
+                        type='submit'
+                        disabled={!this.state.username}>Submit</button>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
+}
 
 class Battle extends React.Component {
     constructor(props){
@@ -20,16 +72,20 @@ class Battle extends React.Component {
             var newState = {
             }
             newState[id + 'Name'] = username;
-            newState[id + 'Image'] = 'https://github.com/' + uername + '.png?size=200';
+            newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
             return newState;
         })
     }
 
     render(){
+        var playerOneName = this.state.playerOneName;
+        var playerTwoName = this.state.playerTwoName;
+
         return (
             <div>
                 <div className="row">
-                    
+                    {!playerOneName && <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit} />}
+                    {!playerTwoName && <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit} />}
                 </div>
             </div>
         )
