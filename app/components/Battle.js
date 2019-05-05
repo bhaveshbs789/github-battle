b082e81235759c500d8ccec487a343484b819923
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-05-05 16:29:06 +0530

    Added state to Battle component with player1 and player2 name and image

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,16 @@
 var React = require('react');
 
 class Battle extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            playerOneName:'',
+            playerTwoName:'',
+            playerOneImage: null,
+            playerTwoImage: null
+        }
+    }
     render(){
         return (
             <div>Battle</div>
