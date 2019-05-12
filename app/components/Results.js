105c833a5cabe4c1fd4b688a9935063ee4219e15
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-05-12 14:20:37 +0530

    Added Loading component with customizability

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var Link = require('react-router-dom').Link;
 var PropTypes = require('prop-types');
 var PlayerPreview = require('./PlayerPreview');
 var PropTypes = require('prop-types');
+var Loading = require('./Loading');
 
 function Profile(props){
     return (
@@ -86,7 +87,7 @@ class Results extends React.Component {
         var loading = this.state.loading;
 
         if(loading === true){
-            return <p>Loading</p>
+            return <Loading />
         }
 
         if(error){
