3aabf5eb178a91f62f7ad673b69a14ddb05bd8ef
Bhavesh Sharma <bhaveshbs789@gmail.com>
2019-05-12 12:36:33 +0530

    Composed functions for player results

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,27 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function PlayerPreview(props){
-    return (
-        <div>
-            <div className='column'>
-                <img className='avatar'
-                    src={props.avatar}
-                    alt={'Avatar for ' + props.username}></img>
-                <h2 className='username'>@{props.username}</h2>
-            </div>
-            <button className='reset' onClick={props.onReset.bind(null, props.id)}>Reset</button>
-        </div>
-    )
-}
-
-PlayerPreview.propTypes = {
-    avatar:PropTypes.string.isRequired,
-    username:PropTypes.string.isRequired,
-    onReset:PropTypes.func.isRequired,
-    id:PropTypes.string.isRequired
-}
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
     constructor(props){
@@ -123,16 +103,20 @@ class Battle extends React.Component {
         return (
             <div>
                 <div className="row">
-                    {!playerOneName && <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit} />}
+                    {!playerOneName && <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit}/>}
                     {playerOneImage !== null && <PlayerPreview avatar={playerOneImage}
                                                     username={playerOneName}
                                                     onReset={this.handleReset}
-                                                    id='playerOne'/>}
-                    {!playerTwoName && <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit} />}
+                                                    >
+                                                    <button className='reset' onClick={this.handleReset.bind(null, 'playerOne')}>Reset</button>
+                                                    </PlayerPreview>}
+                    {!playerTwoName && <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit}/>}
                     {playerTwoImage !== null && <PlayerPreview avatar={playerTwoImage}
                                                     username={playerTwoName}
                                                     onReset={this.handleReset}
-                                                    id='playerTwo'/>}
+                                                    >
+                                                    <button className='reset' onClick={this.handleReset.bind(null, 'playerTwo')}>Reset</button>
+                                                    </PlayerPreview>}
                 </div>
 
                 {/* {playerOneImage && playerTwoImage &&
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,56 @@
 var React = require('react');
 var queryString = require('query-string');
 var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
+var PropTypes = require('prop-types');
+var PlayerPreview = require('./PlayerPreview');
+var PropTypes = require('prop-types');
+
+function Profile(props){
+    return (
+        <PlayerPreview 
+            avatar={props.info.avatar_url}
+            username={props.info.login}>
+            <ul className='space-list-items'>
+                {props.info.name && <li>{props.info.name}</li>}
+                {props.info.location && <li>{props.info.location}</li>}
+                {props.info.company && <li>{props.info.company}</li>}
+                <li>Followers: {props.info.followers}</li>
+                <li>Following: {props.info.following}</li>
+                <li>Public Repos: {props.info.public_repos}</li>
+                {props.info.blog && <li><a href={props.info.blog}>{props.info.blog}</a></li>}
+            </ul>
+        </PlayerPreview>
+    )
+}
+
+function Player(props){
+    return (
+        <div>
+            <h1 className='header'>{props.label}</h1>
+            <h3 style={{textAlign:'center'}}>Score: {props.score}</h3>
+            <Profile info={props.profile}/>
+        </div>
+    )
+}
+
+Player.propTypes = {
+    label: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    profile: PropTypes.object.isRequired
+}
 
 class Results extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
 
     componentDidMount(){
         var players = queryString.parse(this.props.location.search);
@@ -10,13 +58,57 @@ class Results extends React.Component {
             players.playerOneName,
             players.playerTwoName
         ]).then(function(results){
-            console.log(results);
-        })
+            // console.log(results);
+            if(results === null){
+                return this.setState(function(){
+                    return {
+                        error:'Looks like there was an error. Check the usernames again!!',
+                        loading: false
+                    }
+                })
+            }
+
+            this.setState(function(){
+                return {
+                    error: null,
+                    loading: false,
+                    winner: results[0],
+                    loser: results[1]
+                }
+            })
+        }.bind(this));
     }
 
     render(){
+        var error = this.state.error;
+        var winner = this.state.winner;
+        var loser = this.state.loser;
+        var loading = this.state.loading;
+
+        if(loading === true){
+            return <p>Loading</p>
+        }
+
+        if(error){
+            return (
+                <div>
+                    <p>{error}</p>
+                    <Link to='/battle'>Reset</Link>
+                </div>
+            )
+        }
+
         return (
-            <div>Results</div>
+            <div className='row'>
+                <Player 
+                    label='Winner'
+                    score={winner.score}
+                    profile={winner.profile}/>
+                <Player 
+                    label='Loser'
+                    score={loser.score}
+                    profile={loser.profile}/>
+            </div>
         )
     }
 }
